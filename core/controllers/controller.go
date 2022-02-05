00523df7a0d37f52c152a559217c569bd6ed1ce1
G. Medina <github@g-medina.de>
2022-02-05 19:49:06 +0100

    Bee go

--- core/controllers/controller.go
+++ core/controllers/controller.go
@@ -19,8 +19,7 @@ type BaseController struct {
 }
 
 func (this *BaseController) Prepare() {
-	this.Data["Title"] = "Hello"
-
+	this.Data["Title"] = web.BConfig.AppName
 	path := this.Ctx.Request.URL.Path
 	currentNavigation1 := template.GetCurrentNavigation(path, template.Navigation)
 	this.Data["Navigation1"] = template.Navigation
@@ -29,9 +28,7 @@ func (this *BaseController) Prepare() {
 	this.Data["CurrentNavigation2"] = template.GetCurrentNavigation(path, currentNavigation1.SubMenu)
 	this.Data["CurrentNavigation1Index"] = template.GetCurrentNavigationIndex(path, template.Navigation)
 	this.Data["CurrentNavigation2Index"] = template.GetCurrentNavigationIndex(path, currentNavigation1.SubMenu)
-
 }
 
 func (this *BaseController) Finish() {
-	this.Data["Title"] = "Hello"
 }
